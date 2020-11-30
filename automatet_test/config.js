91195fd2b0ab3e22ecccc8c28b333cf975e69412
Rdavid1991 <42180920+Rdavid1991@users.noreply.github.com>
2020-11-29 21:20:05 -0500

    Pruebas jenkins config

--- automatet_test/config.js
+++ automatet_test/config.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 require('electron-chromedriver/chromedriver')
 const webdriver = require( 'selenium-webdriver' );
 const {setDefaultTimeout} = require('cucumber')
 const path = require( 'path' );
-=======
-//require('electron-chromedriver/chromedriver')
-require('chromedriver')
-const webdriver = require('selenium-webdriver');
-const { setDefaultTimeout } = require('cucumber')
-const path = require('path');
->>>>>>> a70bc3fb1c4b0cda1f26ad147abe358d6966d4cb
 
 const driver = new webdriver.Builder()
   // The "9515" is the port opened by chrome driver.
@@ -17,13 +9,8 @@ const driver = new webdriver.Builder()
   .withCapabilities({
     chromeOptions: {
       // Here is the path to your Electron binary.
-<<<<<<< HEAD
       binary: path.join( __dirname, '../passwordencrypt/dist/backup-password-react 0.1.0.exe' ),
       args:['--headless']
-=======
-      //binary: path.join( __dirname, '../passwordencrypt/dist/backup-password-react 0.1.0.exe' ),
-      args: ['--headless','--no-sandbox']
->>>>>>> a70bc3fb1c4b0cda1f26ad147abe358d6966d4cb
     }
   })
   .forBrowser('chrome')
